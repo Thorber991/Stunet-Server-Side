0fdc87fb5e261d395a9c853912d286584db9febc
Thorber991 <66002407+Thorber991@users.noreply.github.com>
2022-05-03 09:14:18 +0200

    Added main Server and Client Files

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+SERVER = input()
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message) 
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+input()
+send("Hey Byddy!")
+input()
+send("Hello Budds")
+input()
+send(DISCONNECT_MSG)
